8855b1d500eb6640ab7feadd65dec35f8c48104f
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-20 18:07:50 -0500

    refactor/Refactoring the printMatrix method

--- src/main/java/org/Staticstructures/Matrix.java
+++ src/main/java/org/Staticstructures/Matrix.java
@@ -10,7 +10,12 @@ public class Matrix {
     }
 
     public static void printMatrix(int[][] matrix) {
-        System.out.println("Matrix: " + Arrays.deepToString(matrix));
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 
     private static int[][] transformMatrix(int[][] matrix, MatrixMap map) {
