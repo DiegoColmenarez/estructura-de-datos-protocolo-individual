5f4085f46194f3da08b975fb9304e1dcfa580eb8
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-21 12:29:07 -0500

    feat/ Creating the Product Class

--- src/main/java/org/exercise7/Product.java
+++ src/main/java/org/exercise7/Product.java
@@ -1,4 +1,40 @@
 package org.exercise7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
+   private String name;
+   private String id;
+   private float price;
+
+    public Product (String id, String name, float price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    static void main() {
+
+        // que no se te olvide borrar esto de aquí, fue una prueba rapida
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("A001", "Laptop Lenovo", 123.3F));
+        productList.add(new Product("A002", "Teclado Mecanico", 23.6F));
+        productList.add(new Product("A003", "Funda de Laptop", 12.3F));
+        productList.add(new Product("A004", "Mouse inalambrico", 15.7F));
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }
