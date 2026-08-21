b6e91df59fdd5da0e0c63821d0abf727e07874f6
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-21 16:27:31 -0500

    feat-refactor/Implementation of the  method and refactoring of the getter methods and the constructor of the  class

--- src/main/java/org/exercise7/Product.java
+++ src/main/java/org/exercise7/Product.java
@@ -1,29 +1,19 @@
 package org.exercise7;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Product {
-   private String name;
-   private String id;
-   private float price;
+   private final String name;
+   private final String id;
+   private final BigDecimal price;
 
-    public Product (String id, String name, float price){
+    public Product (String id, String name, BigDecimal price){
         this.id = id;
         this.name = name;
         this.price = price;
     }
-
-    static void main() {
-
-        // que no se te olvide borrar esto de aquí, fue una prueba rapida
-        List<Product> productList = new ArrayList<>();
-        productList.add(new Product("A001", "Laptop Lenovo", 123.3F));
-        productList.add(new Product("A002", "Teclado Mecanico", 23.6F));
-        productList.add(new Product("A003", "Funda de Laptop", 12.3F));
-        productList.add(new Product("A004", "Mouse inalambrico", 15.7F));
-    }
-
     public String getName() {
         return name;
     }
@@ -32,7 +22,25 @@ public class Product {
         return id;
     }
 
-    public float getPrice() {
+    public BigDecimal getPrice() {
         return price;
+    }
+    static void main() {
+
+        // que no se te olvide borrar esto de aquí, fue una prueba rapida
+        List<Product> productList = new ArrayList<>();
+
+        Product product1 = new Product("A001", "Laptop Lenovo", new BigDecimal("123.3"));
+        Product product2 = new Product("A002", "Teclado Mecanico", new BigDecimal("23.6"));
+        Product product3 = new Product("A003", "Funda de Laptop", new BigDecimal("12.3"));
+
+        productList.add(product1);
+        productList.add(product2);
+        productList.add(product3);
+
+        List<SalesLine> salesLineList = new ArrayList<>();
+        SalesLine salesProduct1 = new SalesLine(product1, 6);
+        salesLineList.add(salesProduct1);
+
     }
 }
--- src/main/java/org/exercise7/SalesLine.java
+++ src/main/java/org/exercise7/SalesLine.java
@@ -1,14 +1,15 @@
 package org.exercise7;
 
+import java.math.BigDecimal;
+
 public class SalesLine {
-    private Product product;
-    private int salesNum;
+    private final Product product;
+    private final int salesNum;
 
     public SalesLine(Product product, int salesNum){
         this.product = product;
         this.salesNum = salesNum;
     }
-
 
     public Product getProduct() {
         return product;
@@ -16,5 +17,11 @@ public class SalesLine {
 
     public int getSalesNum() {
         return salesNum;
+    }
+
+    public BigDecimal getLineTotal(){
+        BigDecimal productPrice = new BigDecimal(String.valueOf(product.getPrice()));
+        BigDecimal salesNum = new BigDecimal(getSalesNum());
+        return productPrice.multiply(salesNum);
     }
 }
