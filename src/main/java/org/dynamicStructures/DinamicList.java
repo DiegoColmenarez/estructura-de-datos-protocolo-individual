9873545e06994b0635471bf3289f6cc4a44ea579
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-23 15:23:58 -0500

    feat/ Creating the DinamicList class constructor

--- src/main/java/org/dynamicStructures/DinamicList.java
+++ src/main/java/org/dynamicStructures/DinamicList.java
@@ -1,4 +1,12 @@
 package org.dynamicStructures;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DinamicList {
+    private List<Integer> integerList = new ArrayList<>();
+
+    public  DinamicList(List<Integer> integerList){
+        this.integerList = integerList;
+    }
 }
