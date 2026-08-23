724c145c402718107b001ac06a2c19eecfa875a0
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-23 16:01:06 -0500

    feat/ Implementation of the transformList method

--- src/main/java/org/dynamicStructures/DinamicList.java
+++ src/main/java/org/dynamicStructures/DinamicList.java
@@ -2,11 +2,20 @@ package org.dynamicStructures;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
 
 public class DinamicList {
     private List<Integer> integerList = new ArrayList<>();
 
     public  DinamicList(List<Integer> integerList){
         this.integerList = integerList;
+    }
+
+    private List<Integer> transformList(List<Integer> integerList, BiFunction<Integer, Integer, Integer> operation){
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < integerList.size(); i++) {
+            result.add(operation.apply(integerList.get(i), i ));
+        }
+        return result;
     }
 }
